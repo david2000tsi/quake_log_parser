8ff393e941032ac7641877feb050dfee3ca3874c
David Alves <david2000tsi@gmail.com>
2019-04-29 22:18:18 -0300

    Added methods to recovery players and kills from one match

--- Parser.php
+++ Parser.php
@@ -6,6 +6,8 @@ class Parser
 	CONST LOG_FILE_NAME = "games.log";
 	CONST LOG_KEY_INIT_GAME = "InitGame";
 	CONST LOG_KEY_SHUTDOWN_GAME = "ShutdownGame";
+	CONST LOG_KEY_KILL = "Kill";
+	CONST LOG_KEY_PLAYER = "ClientUserinfoChanged";
 
 	private $logArr;
 
@@ -63,5 +65,66 @@ class Parser
 		}
 
 		return $matchList;
+	}
+
+	// Returns all kills from one match.
+	// array $match An array containing a match.
+	// Returns an array with only kills.
+	public function getKillsFromMatch(array $match)
+	{
+		$kills = array();
+
+		if(count($match) == 0)
+		{
+			return false;
+		}
+
+		foreach($match as $line)
+		{
+			if(strpos($line, self::LOG_KEY_KILL))
+			{
+				array_push($kills, $line);
+			}
+		}
+
+		return $kills;
+	}
+
+	// Returns all players from one match.
+	// array $match An array containing a match.
+	// Returns an array with only players.
+	public function getPlayersFromMatch(array $match)
+	{
+		$players = array();
+
+		if(count($match) == 0)
+		{
+			return false;
+		}
+
+		foreach($match as $line)
+		{
+			if(strpos($line, self::LOG_KEY_PLAYER))
+			{
+				// "3:32 ClientUserinfoChanged: 2 n\Oootsimo\t\0\model\razor/id\hmodel\razor/id\g_redteam\\g_blueteam\\c1\3\c2\5\hc\100\w\0\l\0\tt\0\tl\0"
+				//  strpos returns 30             ^
+				// We need to recover only 'Oootsimo' from string.
+				$position = strpos($line, "n\\");
+				$position += 2; // Adding two more bytes (n\).
+				$player = "";
+
+				for($i = 0; $line[$position] != "\\"; $i++, $position++)
+				{
+					$player[$i] = $line[$position];
+				}
+
+				array_push($players, $player);
+			}
+		}
+
+		// Removing duplicated players.
+		$players = array_unique($players);
+
+		return $players;
 	}
 }
--- ParserTest.php
+++ ParserTest.php
@@ -8,16 +8,19 @@ final class ParserTest extends TestCase
 {
 	private $parser;
 
+	// Instantiates Parser class.
 	public function setUp()
 	{
 		$this->parser = new Parser();
 	}
 
+	// Destroys Parser class.
 	public function tearDown()
 	{
 		$this->parser = null;
 	}
 
+	// Load all file content.
 	public function testLoadLogContent()
 	{
 		$logContent = $this->parser->getLogContent();
@@ -26,11 +29,40 @@ final class ParserTest extends TestCase
 		$this->assertEquals($type, "array");
 	}
 
+	// List matches in subarrays.
 	public function testGetMatchList()
 	{
 		$matchList = $this->parser->getMatchList();
 		$type = gettype($matchList);
 
 		$this->assertEquals($type, "array");
+	}
+
+	// Gets all kills from one match
+	public function testGetKillsFromMatch()
+	{
+		$matchList = $this->parser->getMatchList();
+		if(count($matchList))
+		{
+			$kills = $this->parser->getKillsFromMatch($matchList[5]);
+			$type = gettype($kills);
+
+			$this->assertEquals($type, "array");
+
+			//var_dump($kills);
+		}
+	}
+
+	// Gets all players from one match
+	public function testGetPlayersFromMatch()
+	{
+		$matchList = $this->parser->getMatchList();
+		if(count($matchList))
+		{
+			$players = $this->parser->getPlayersFromMatch($matchList[5]);
+			$type = gettype($players);
+
+			$this->assertEquals($type, "array");
+		}
 	}
 }
